eedaf981abc526df89e86a54c5c7515fd84ef9db
Skyline <maratsamat18@gmail.com>
2026-07-09 18:32:30 +0500

    feat/style(frontend-17/18): Добавление Layout(шапки, бокового меню, главного контента), обновление роутера и обновление структуры проекта (#20)

--- frontend/src/app/routes/AppRouter.tsx
+++ frontend/src/app/routes/AppRouter.tsx
@@ -5,18 +5,24 @@ import { MessagesPage } from "@pages/messages";
 import { NotFoundPage } from "@pages/not-found";
 import { ProfilePage } from "@pages/profile";
 import { RegisterPage } from "@pages/register";
+import { Layout } from "@widgets/layout/ui/Layout";
 import { Route, Routes } from "react-router-dom";
 
 export const AppRouter = () => {
     return (
         <Routes>
-            <Route path="/" element={<HomePage />} />
+            
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/profile/:id" element={<ProfilePage />} />
-            <Route path="/friends" element={<FriendsPage />} />
-            <Route path="/messages" element={<MessagesPage />} />
+                        
+            <Route element={<Layout />}>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+                <Route path="/profile/:id" element={<ProfilePage />} />
+                <Route path="/friends" element={<FriendsPage />} />
+                <Route path="/messages" element={<MessagesPage />} />
+            </Route>
+
             <Route path="*" element={<NotFoundPage />} />
         </Routes>
     );
