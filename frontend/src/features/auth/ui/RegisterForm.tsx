3e755bf38192c078bff4f27871b1af0658930e28
Shade-Angel <maratsamat18@gmail.com>
2026-07-11 09:54:57 +0500

    feat(frontend-32): создал ui компонент регистрации

---
+++ frontend/src/features/auth/ui/RegisterForm.tsx
@@ -0,0 +1,120 @@
+import { useNavigate } from "react-router-dom";
+import z from "zod";
+import { useAuth } from "../model";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { authApi } from "../api";
+import { Alert, Box, Button, TextField, Typography } from "@mui/material";
+
+const registerSchema = z.object({
+    name: z.string().min(2, 'Имя должно содержать минимум 2 буквы'),
+    email: z.string().email('Некорректный email'),
+    password: z.string().min(8, 'Пароль должен содержать минимум 8 символов'),
+    secondPassword: z.string(),
+}).refine((data) => data.password === data.secondPassword, {
+    message: 'Введенные пароли не совпадают!',
+    path: ['secondPassword'],
+});
+
+type RegisterFormData = z.infer<typeof registerSchema>;
+
+export const RegisterForm = () => {
+    const navigate = useNavigate();
+    const { login } = useAuth();
+    const [error, setError] = useState<string | null>(null);
+
+    const {
+        register,
+        handleSubmit,
+        formState: {errors, isSubmitting},
+    } = useForm<RegisterFormData>({
+        resolver: zodResolver(registerSchema),
+    });
+
+    const onSubmit = async (data: RegisterFormData) => {
+        try {
+            setError(null);
+
+            const response = await authApi.register({
+                email: data.email,
+                password: data.password,
+                name: data.name,
+            });
+
+            const { token } = response.data;
+            login(token);
+            navigate('/');
+        } catch (err: unknown){
+            const error = err as { response?: { data?: { message?: string } } };
+            setError(error.response?.data?.message || 'Ошибка при регистрациию Проверьте введенные данные.');
+        }
+    };
+
+    return(
+        <Box
+            component='form'
+            onSubmit={handleSubmit(onSubmit)}
+            sx={{
+                maxWidth: 400,
+                mx: 'auto',
+                mt: 8,
+                p: 3,
+                boxShadow: 3,
+                borderRadius: 2,
+            }}
+        >
+            <Typography variant="h5" gutterBottom sx={{textAlign: 'center'}}>
+                Регистрация
+            </Typography>
+
+            {error && <Alert severity="error" sx={{mb: 2}}>{error}</Alert>}
+
+            <TextField 
+                label='Имя'
+                fullWidth
+                margin="normal"
+                {...register('name')}
+                error={!!errors.name}
+                helperText={errors.name?.message}
+            />
+
+            <TextField 
+                label='Email'
+                fullWidth
+                margin="normal"
+                {...register('email')}
+                error={!!errors.name}
+                helperText={errors.name?.message}
+            />
+
+            <TextField 
+                label='Password'
+                fullWidth
+                margin="normal"
+                {...register('password')}
+                error={!!errors.name}
+                helperText={errors.name?.message}
+            />
+
+            <TextField 
+                label='ConfirmPassword'
+                fullWidth
+                margin="normal"
+                {...register('secondPassword')}
+                error={!!errors.name}
+                helperText={errors.name?.message}
+            />
+
+            <Button
+                type="submit"
+                variant="contained"
+                fullWidth
+                disabled
+                sx={{mt: 3, py: 1.5}}
+            >
+                {isSubmitting ? 'Регистрация' : 'Зарегистрироваться'}
+            </Button>
+        </Box>
+    );
+};
--- frontend/src/pages/register/RegisterPage.tsx
+++ frontend/src/pages/register/RegisterPage.tsx
@@ -1,3 +1,25 @@
+import { RegisterForm } from "@features/auth/ui";
+import { Box, Link, Typography } from "@mui/material";
+import { Link  as RouterLink} from "react-router-dom";
+
 export const RegisterPage = () => {
-    return <div>Страница регистрации</div>;
+    return (
+        <Box sx={{
+            minHeight: '80vh',
+            display: 'flex',
+            alignItems: 'center'
+        }}>
+            <Box sx={{width: '100%'}}>
+                <RegisterForm />
+                <Box sx={{ mt: 2, textAlign: 'center'}}>
+                    <Typography variant="body2" color="text.secondary">
+                        Уже есть аккаунт?{' '}
+                        <Link component={RouterLink} to='/login' underline="hover">
+                            Войти
+                        </Link>
+                    </Typography>
+                </Box>
+            </Box>
+        </Box>
+    );
 };
